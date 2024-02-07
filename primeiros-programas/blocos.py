0d2bb912f50c60f4ceaba617fc31038ca4d09981
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-07 15:54:17 -0300

    Sintaxe

---
+++ primeiros-programas/blocos.py
@@ -0,0 +1,6 @@
+idade = 25
+
+if idade < 18:
+    print("Menor de Idade")
+else:
+    print("Maior de idade")    
---
+++ primeiros-programas/sintaxe.py
@@ -0,0 +1,13 @@
+# Comentário de 1 linha
+
+"""Este e um comentario de varias linhas
+   Este e um comentario de varias linhas
+   Este e um comentario de varias linhas
+   Este e um comentario de varias linhas 
+"""
+
+
+nome = "Robson" + \
+" De Jesus"
+
+print(nome)
