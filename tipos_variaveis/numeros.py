dff90a9aa7648441cc44f7c8974b8fc14c783f86
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-07 16:08:16 -0300

    Numeros e tipos

---
+++ tipos_variaveis/numeros.py
@@ -0,0 +1,13 @@
+# Inteiro 
+numero_inteiro = 3
+print("Inteiro = ", numero_inteiro)
+
+# Real com ponto flutuante 
+numero_real = 3.14
+print("Real com ponto flutuante =", numero_real)
+
+
+# Type() tipo da variaveis 
+print("Tipo = ", type(numero_inteiro))
+print("Tipo = ", type(numero_real))
+
