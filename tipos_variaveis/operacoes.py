9b168c36c0a7910906efb7f6943fe60af98540b8
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-07 16:19:51 -0300

    Operações 2

---
+++ tipos_variaveis/operacoes.py
@@ -0,0 +1,37 @@
+#soma 
+soma = 1 + 1
+print("1 + 1 =", soma)
+
+
+#subtração
+subracao = 1 - 1
+print(subracao)
+
+
+#multiplicação 
+multiplicacao = 2 * 2
+print(multiplicacao)
+
+
+# Divisão 
+divisao = 5 / 2
+print(divisao)
+print(type(divisao))
+print("Valor em inteiro: ", int(divisao))
+
+
+print("-----------------------")
+print("Divisão inteira")
+divisaoIn = 5 // 2
+print("Divisão inteira", divisaoIn)
+print("-----------------------")
+
+
+#float para converter
+print("Float: ", float(soma))
+
+
+
+#Modulo 
+modulo = 5 % 2
+print("modulo = ", modulo)
